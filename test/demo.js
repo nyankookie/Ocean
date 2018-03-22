67e46522173df9dad8a8fd2a981f96de38f8cbb8
MoeMoeMortimer <nyankochan611@gmail.com>
2018-03-22 23:35:06 +0800

    add Current

--- test/demo.js
+++ test/demo.js
@@ -45,27 +45,13 @@ $.getJSON('surface-currents-global.json', function (data) {
 			displayEmptyString: 'No current data'
 		},
 		data: data,
-		maxVelocity: 10
+		maxVelocity: 15
 	});
 	layerControl.addOverlay(velocityLayer, 'Current - Global');
 });
 
 
-$.getJSON('wind-global.json', function (data) {
 
-	var velocityLayer = L.velocityLayer({
-		displayValues: true,
-		displayOptions: {
-			velocityType: 'Global Wind',
-			displayPosition: 'bottomleft',
-			displayEmptyString: 'No wind data'
-		},
-		data: data,
-		maxVelocity: 15
-	});
-
-	layerControl.addOverlay(velocityLayer, 'Wind - Global');
-});
 
 
 
